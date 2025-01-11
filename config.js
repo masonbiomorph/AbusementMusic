8a5a8b07bb2d12d10563fd2c3f009f6ed2d30972
masonbiomorph <40918350+masonbiomorph@users.noreply.github.com>
2025-01-10 21:18:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ module.exports = {
 
   nodes: [
     {
-        name: "INZEWORLD.COM (DE)",
-        host: "lava.inzeworld.com",
-        port: 3128,
-        password: "saher.inzeworld.com",
+        name: "Public Lavalink v4 NonSSL",
+        host: "lava-v4.ajieblogs.eu.org",
+        port: 80,
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
