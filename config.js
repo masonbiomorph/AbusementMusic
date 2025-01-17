f68d8a35a68e8bf76fcea71bd8915b6654d26f2a
masonbiomorph <40918350+masonbiomorph@users.noreply.github.com>
2025-01-17 17:56:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ module.exports = {
 
   nodes: [
     {
-        name: "INZEWORLD.COM (DE)",
-        host: "lava.inzeworld.com",
-        port: 3128,
-        password: "saher.inzeworld.com",
+        name: "Muzykant v4",
+        host: "lavalink_v4.muzykant.xyz",
+        port: 80,
+        password: "https://discord.gg/v6sdrD9kPh",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
